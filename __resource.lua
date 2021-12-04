b9577670644d059fdbdc46d03451004924807495
RosdeeStudio <rosdee.studio@gmail.com>
2021-12-04 20:53:59 +0200

    first commit

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+	'client.lua',
+	'@es_extended/locale.lua',
+	'locales/fi.lua',
+	'locales/en.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/fi.lua',
+	'locales/en.lua',
+	'config.lua'
+}
