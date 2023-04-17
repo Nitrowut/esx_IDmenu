d7f64ee21e6b8c9082af0296b723ba4a8aa69be6
Nitrowut <90637471+Nitrowut@users.noreply.github.com>
2023-04-18 00:33:56 +0300

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,55 @@
+Locales['en'] = {
+	-- Citizen Interactions
+	["citizen_interactions"] 	= "ID Menu",
+	["vehicle_menu"]			= "Vehicle menu",
+	["id_card"]					= "ID Card",
+	["id_card2"]				= "Driving license",
+	["id_card3"]				= "Weapon License",
+	["pee"]						= "Pee",
+	["poop"]					= "Poop",
+	["blindfold"]				= "On/Off blindfold",
+	["nobody_near"]				= "No players nearby",
+	-- ID Meny
+	["check_id"]				= "Look ID card",
+	["show_id"]					= "Show ID card",
+	["check_id2"]				= "Look Driving License",
+	["show_id2"]				= "Show Driving License",
+	["check_id3"]				= "Look Weapon License",
+	["show_id3"]				= "Show Weapon License",
+	["id_menu"]					= "ID card",
+	["dl_menu"]					= "Driving License",
+	["gl_menu"]					= "Weapon License",
+	-- Fordonsmeny
+	["engine"]					= "Motor",
+	["doors"]					= "Doors",
+	["window"]					= "Windows",
+	["cruise_control"]			= "Cruise Control",
+	["shuff"]					= "Change a seat",
+	["turned_off_engine"]		= "You turned off the engine",
+	["turned_on_engine"]		= "You started the engine",
+	-- Dörrar
+	["hood"]					= "Hood",
+	["trunk"]					= "Trunk",
+	["front_left_door"]			= "Left front door",
+	["front_right_door"]		= "Right front door",
+	["back_left_door"]			= "Left rear door",
+	["back_right_door"]			= "Right rear door",
+	["closed_hood"]				= "You closed the hood!",
+	["opened_hood"]				= "You opened the hood!",
+	["closed_door"]				= "You opened the door!",
+	["opened_door"]				= "You closed the door!",
+	-- Fönster meny
+	["window_menu"]				= "Window menu",
+	["front_left_window"]		= "Left right window",
+	["front_right_window"]		= "Right front window",
+	["back_left_window"]		= "Left rear window",
+	["back_right_window"]		= "Right rear door",
+	["opened_window"]			= "You closed the window",
+	["closed_wondow"]			= "You opened the window",
+	-- Fartbegränsare
+	["disabled"]				= "Disabled",
+	["disabled_cruise_control"]	= "You turn off Cruise Control",
+	-- Seatbelt
+	["seatbelt_on"]				= "Fasten your seat belt!",
+	["seatbelt_off"]			= "Removed seat belt!"
+}
